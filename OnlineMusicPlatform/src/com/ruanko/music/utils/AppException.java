b4aea8fba0ad7f9bf3b9efa5f4e5b44fd5cdbfb9
Sagen <32996212+969256235@users.noreply.github.com>
2018-07-02 11:19:24 +0800

    自定义异常类
    自定义异常类AppException

---
+++ OnlineMusicPlatform/src/com/ruanko/music/utils/AppException.java
@@ -0,0 +1,30 @@
+package com.ruanko.music.utils;
+
+/**
+ * 自定义异常类
+ * @author Zuo
+ */
+public class AppException extends Exception{
+
+	private int exceptionCode; //异常编号
+	private String message;   //异常消息
+	
+	public AppException(String message_){
+		this.message = message_;
+	}
+	
+	public AppException(String message_, int exceptionCode_){
+		this.exceptionCode = exceptionCode_;
+		this.message = message_;
+	}
+
+	public int getExceptionCode() {
+		return exceptionCode;
+	}
+
+	public String getMessage() {
+		String detailMessage = "Detail message: exception" + this.exceptionCode + " " + message;
+		return detailMessage;
+	}
+	
+}
