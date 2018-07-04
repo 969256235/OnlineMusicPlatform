bfb71326de02e419402ef99a2a54ca5e000d0c3d
Sagen <32996212+969256235@users.noreply.github.com>
2018-07-04 17:45:27 +0800

    MusicDao接口增加

--- OnlineMusicPlatform/src/com/ruanko/music/dao/MusicDao.java
+++ OnlineMusicPlatform/src/com/ruanko/music/dao/MusicDao.java
@@ -1,8 +1,11 @@
 package com.ruanko.music.dao;
 
 import java.util.ArrayList;
-
+import com.ruanko.music.model.Album;
+import com.ruanko.music.model.Artist;
+import com.ruanko.music.model.Music;
 import com.ruanko.music.model.MusicBusiModel;
+import com.ruanko.music.model.Popularity;
 import com.ruanko.music.utils.AppException;
 
 /**
@@ -48,4 +51,55 @@ public interface MusicDao {
 	 * @throws AppException
 	 */
 	public ArrayList<MusicBusiModel> getMusicByAlbum(String alb_name) throws AppException;
+	
+	/**
+	 * 按名字检索歌手
+	 * @param name
+	 * @return 符合条件的歌手列表
+	 * @throws AppException
+	 */
+	public ArrayList<Artist> getArtistByName(String name) throws AppException;
+	
+	/**
+	 * 按名字检索专辑
+	 * @param name
+	 * @return 符合条件的专辑列表
+	 * @throws AppException
+	 */
+	public ArrayList<Album> getAlbumByName(String name) throws AppException;
+	
+	/**
+	 * 添加音乐
+	 * @param music 音乐基础模型
+	 * @throws AppException
+	 */
+	public boolean addMusic(Music music) throws AppException;
+	
+	/**
+	 * 添加艺人
+	 * @param artist
+	 * @throws AppException
+	 */
+	public boolean addArtist(Artist artist) throws AppException;
+	
+	/**
+	 * 添加专辑
+	 * @param album
+	 * @throws AppException
+	 */
+	public boolean addAlbum(Album album) throws AppException;
+	
+	/**
+	 * 添加热度
+	 * @param pop
+	 * @throws AppException
+	 */
+	public boolean addPopularity(Popularity pop) throws AppException;
+	
+	/**
+	 * 删除音乐
+	 * @param id 欲删除的音乐的id
+	 * @throws AppException
+	 */
+	public boolean delMusic(String id) throws AppException;
 }
--- OnlineMusicPlatform/src/com/ruanko/music/dao/impl/MusicDaoImp.java
+++ OnlineMusicPlatform/src/com/ruanko/music/dao/impl/MusicDaoImp.java
@@ -1,9 +1,12 @@
 package com.ruanko.music.dao.impl;
 
 import java.util.ArrayList;
-
 import com.ruanko.music.dao.MusicDao;
+import com.ruanko.music.model.Album;
+import com.ruanko.music.model.Artist;
+import com.ruanko.music.model.Music;
 import com.ruanko.music.model.MusicBusiModel;
+import com.ruanko.music.model.Popularity;
 import com.ruanko.music.utils.AppException;
 
 /**
@@ -39,6 +42,48 @@ public class MusicDaoImp implements MusicDao{
 	public ArrayList<MusicBusiModel> getMusicByAlbum(String alb_name) throws AppException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public ArrayList<Artist> getArtistByName(String name) throws AppException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Album> getAlbumByName(String name) throws AppException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addMusic(Music music) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addArtist(Artist artist) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAlbum(Album album) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addPopularity(Popularity pop) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delMusic(String id) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
