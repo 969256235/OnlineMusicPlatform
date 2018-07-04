6c7fe1f7c98d6bc9a3f42f190a7ad4dd09f58d8f
Sagen <32996212+969256235@users.noreply.github.com>
2018-07-04 09:42:59 +0800

    部分类与接口

---
+++ OnlineMusicPlatform/src/com/ruanko/music/dao/MusicDao.java
@@ -0,0 +1,8 @@
+package com.ruanko.music.dao;
+
+/**
+ * 音乐数据访问层接口
+ */
+public interface MusicDao {
+
+}
---
+++ OnlineMusicPlatform/src/com/ruanko/music/dao/impl/UserDaoImp.java
@@ -0,0 +1,24 @@
+package com.ruanko.music.dao.impl;
+
+import com.ruanko.music.dao.UserDao;
+import com.ruanko.music.model.User;
+import com.ruanko.music.utils.AppException;
+
+/**
+ * 用户数据访问层实现类
+ */
+public class UserDaoImp implements UserDao{
+
+	@Override
+	public boolean isExist(String account) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean save(User user) throws AppException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
